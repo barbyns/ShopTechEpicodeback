0ed76a35bbc2c57396002010952a2c864b334808
Barbara <barbyns95@gmail.com>
2025-07-19 22:55:31 +0200

    add

---
+++ src/main/java/ShopTech/ShopTechEpicode/ShopTechEpicodeApplication.java
@@ -0,0 +1,13 @@
+package ShopTech.ShopTechEpicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTechEpicodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopTechEpicodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ShopTech/ShopTechEpicode/ShopTechEpicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ShopTech.ShopTechEpicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTechEpicodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
