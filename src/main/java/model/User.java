bd6136976f98bfe0aaf2a920eb2babaf43837045
Barbara <barbyns95@gmail.com>
2025-07-19 23:06:57 +0200

    change user

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -11,4 +11,14 @@ import java.util.Set;
 @Builder
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles")
+    private Set <String> roles;
 }
